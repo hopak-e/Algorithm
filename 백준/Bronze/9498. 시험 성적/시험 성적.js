2b1b5d2e7a68930fcada2f44c92b75dec213b860
hopak-e <97100045+hopak-e@users.noreply.github.com>
2022-04-10 19:10:17 +0900

    [Bronze IV] Title: 시험 성적, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,20 @@
+const input = require('fs').readFileSync('/dev/stdin');
+
+const a = Number(input);
+
+if(a>=90){
+    console.log('A')
+} else if(89>=a && a>=80){
+    console.log('B')
+} else if(79>=a && a>=70){
+    console.log('C')
+} else if(69>=a && a>=60){
+    console.log('D')
+} else {
+    console.log('F')
+};
+    
+    
+    
+    
+    
