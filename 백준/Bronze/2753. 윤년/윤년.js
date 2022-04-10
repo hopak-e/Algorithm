c26ff1b9fec5438b6e824360f5ea02c7cc86a132
hopak-e <97100045+hopak-e@users.noreply.github.com>
2022-04-10 19:21:17 +0900

    [Bronze IV] Title: 윤년, Time: 132 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+
+let a = parseInt(input);
+
+if((a%4===0 && a%100!==0) || a%400===0){
+    console.log("1");
+}else{
+    console.log("0");
+}
